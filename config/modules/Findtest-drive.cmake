44cd68b6eaf5e9d89f881f06a213d839fdc829fb
Philipp Pracht <research@philipp-pracht.de>
2025-01-24 18:15:04 +0100

    remove misleading comments

--- config/modules/Findtest-drive.cmake
+++ config/modules/Findtest-drive.cmake
@@ -1,18 +1,18 @@
-# This file is part of crest.
+# This file is part of libpvol.
 # SPDX-Identifier: LGPL-3.0-or-later
 #
-# crest is free software: you can redistribute it and/or modify it under
+# libpvol is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# crest is distributed in the hope that it will be useful,
+# libpvol is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with crest.  If not, see <https://www.gnu.org/licenses/>.
+# along with libpvol.  If not, see <https://www.gnu.org/licenses/>.
 
 set(_lib "test-drive")
 set(_pkg "TEST-DRIVE")
